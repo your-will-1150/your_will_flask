3bbef9304056bc8583f67e035aa445eb205dba06
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-07-25 15:30:04 -0400

    deploying

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 import os
 
 from app.main import create_app, db
-from app import api
+from app import blueprint
 
 application = create_app(os.getenv('FLASK_ENV'))
-application.register_blueprint(api)
+application.register_blueprint(blueprint)
 
 application.app_context().push()
 
