0599dfb8a2c927a7f7906b11e32cd58a0a766a1b
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-07-26 16:39:21 -0400

    added to item model

--- app/main/model/item.py
+++ app/main/model/item.py
@@ -13,6 +13,8 @@ class Item(db.Model):
     title = db.Column(db.String(32), nullable=False)
     content = db.Column(db.String(128), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    gender = db.Column(db.String(128), nullable=False)
+    category = db.Column(db.String(128), nullabel=False) 
     listed = db.Column(db.Boolean, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
     modified_at = db.Column(db.DateTime, nullable=False)
